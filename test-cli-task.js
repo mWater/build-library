309efbfc4311790ba9d89456489ebd4d6b564498
Clayton Grassick <clayton@claytronics.org>
2019-11-18 09:20:45 -0500

    Add tsx

--- test-cli-task.js
+++ test-cli-task.js
@@ -13,7 +13,7 @@ module.exports = function() {
   require('ignore-styles')
   require("handlebars")
 
-  var files = glob.sync("test/**/*Tests.coffee").concat(glob.sync("test/**/*Tests.ts"))
+  var files = glob.sync("test/**/*Tests.coffee").concat(glob.sync("test/**/*Tests.ts")).concat(glob.sync("test/**/*Tests.tsx"))
 
   for (var file of files) {
     mocha.addFile(file)
